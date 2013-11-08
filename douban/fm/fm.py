daf8a64148ea3b438e1d8e1a6f0ae447b5ca5cc6
MaGuowei <imaguowei@gmail.com>
2013-11-08 17:26:07 +0800

    update

--- douban/fm/fm.py
+++ douban/fm/fm.py
@@ -1,21 +1,50 @@
 from urllib.request import urlopen, HTTPError
 from urllib import parse
+from json import JSONDecoder
 
-#登陆认证页面
-login_url = 'http://www.douban.com/j/app/login'
 
-channel_list = 'http://www.douban.com/j/app/radio/channels'
+class Fm():
+    #认证地址
+    login_url = 'http://www.douban.com/j/app/login'
+    #频道地址
+    channel_list_url = 'http://www.douban.com/j/app/radio/channels'
+    #歌曲列表
+    music_list = 'http://www.douban.com/j/app/radio/people'
+    #登陆数据
+    data = {
+        'app_name': 'radio_desktop_win',
+        'version': 100,
+        'email': '',
+        'password': '',
+    }
 
-data = {
-    'app_name': 'radio_desktop_win',
-    'version': 100,
-    'email': '',
-    'password': '',
-}
+    def __init__(self, email, password):
+        self.data['email'] = email
+        self.data['password'] = password
+        self.data = parse.urlencode(self.data).encode()
+        self.oauth = ''
+        self.channel_list = ''
+        self.get_token()
+        self.get_channel_list()
 
-data = parse.urlencode(data).encode()
-try:
-    r = urlopen(login_url, data)
-    print(r.read().decode())
-except HTTPError as err:
-    print(err)
+    def get_token(self):
+        try:
+            r = urlopen(self.login_url, self.data)
+            self.oauth = r.read().decode()
+        except HTTPError as err:
+            print(err)
+
+    def get_channel_list(self):
+        try:
+            r = urlopen(self.channel_list_url)
+            json = JSONDecoder()
+            self.channel_list = json.decode(r.read().decode())
+        except HTTPError as err:
+            print(err)
+
+fm = Fm('', '')
+
+channels = fm.channel_list['channels']
+
+for channel in channels:
+    print(channel)
