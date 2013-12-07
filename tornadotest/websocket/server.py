4a36d454c64e47ee4893a1ea22d9f6b6c98a771d
MaGuowei <imaguowei@gmail.com>
2013-12-07 20:31:59 +0800

    update

--- tornadotest/websocket/server.py
+++ tornadotest/websocket/server.py
@@ -4,28 +4,30 @@ import tornado.httpserver
 import tornado.ioloop
 
 
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('hello world')
+class TalkHandler(tornado.websocket.WebSocketHandler):
+    message = []
 
-
-class NowHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        self.write_message('connect')
+        self.write_message('已与服务器建立连接')
 
     def on_message(self, message):
-        self.write_message(message + 'hello')
+        self.message.append(message)
+        for msg in self.message:
+            self.write_message(msg)
+            print(message)
+
+    def on_close(self):
+        self.write_message('已与服务器断开连接')
 
 
 app = tornado.web.Application(
     handlers=[
-        (r"/", IndexHandler),
-        (r"/now", NowHandler),
+        (r"/talk", TalkHandler),
     ]
 )
 
 
 if __name__ == '__main__':
-    tornado.httpserver.HTTPServer(app).listen(10000)
+    tornado.httpserver.HTTPServer(app).listen(10005)
     tornado.ioloop.IOLoop.instance().start()
 
---
+++ tornadotest/websocket/static/js/socket.js
@@ -0,0 +1,38 @@
+window.onload = init;
+
+function init() {
+    var username = prompt("输入用户名")
+    var webSocketUri = "ws://localhost:10005/talk";
+    var input = document.getElementById('input');
+    socket = new WebSocket(webSocketUri);
+
+    input.onchange = function(){
+        var message = username + ':' + input.value;
+        socket.send(message);
+        input.value = '';
+    }
+
+    socket.onopen = function(event){
+
+    }
+    socket.onclose = function(event){
+
+    }
+
+    socket.onerror = function(event){
+
+    }
+
+    socket.onmessage = function(event){
+        var message = event.data;
+        var node = document.createTextNode(message);
+        var div = document.createElement("div");
+        div.appendChild(node);
+        document.body.insertBefore(div, input);
+        input.scrollIntoView();
+    }
+
+}
+window.onclose = function(){
+    alert('hello world');
+}
