a29eca4e0112313369e9cd1569c8068a5d8423c4
MaGuowei <imaguowei@gmail.com>
2013-12-09 15:05:50 +0800

    update

--- bing/image.py
+++ bing/image.py
@@ -4,28 +4,48 @@
 from xml.etree import ElementTree
 from urllib.request import urlopen
 from urllib.parse import urlencode
+import json
 
 
 class BingImage():
     #api接口地址
     url = 'http://cn.bing.com/HPImageArchive.aspx?'
 
-    def __init__(self, idx, n):
+    def __init__(self, idx=0, n=1):
         self.idx = idx
         self.n = n
         self.data = self.get_data()
 
     def get_image(self):
+        """获取 image
+        """
         return 'http://s.cn.bing.net' + self.data['url']
 
     def get_date(self):
+        """image 日期
+        """
         return self.data['enddate']
 
     def get_copyright(self):
+        """image 描述
+        """
         return self.data['copyright']
 
     def get_copyright_link(self):
+        """image 搜索连接
+        """
         return self.data['copyrightlink']
+
+    def get_json_format(self):
+        """返回json格式数据
+        """
+        data = {
+            'image': self.get_image(),
+            'date': self.get_date(),
+            'copyright': self.get_copyright(),
+            'copyrightlink': self.get_copyright_link(),
+        }
+        return json.dumps(data)
 
     def get_data(self):
         re = self._fetch()
@@ -45,9 +65,10 @@ class BingImage():
         return self.url + urlencode(data)
 
 if __name__ == '__main__':
-    image = BingImage(0, 1)
-    print(image.get_image())
-    print(image.get_copyright())
-    print(image.get_copyright_link())
-    print(image.get_date())
+    bing = BingImage(0, 1)
+    print(bing.get_image())
+    print(bing.get_copyright())
+    print(bing.get_copyright_link())
+    print(bing.get_date())
+    print(bing.get_json_format())
 
