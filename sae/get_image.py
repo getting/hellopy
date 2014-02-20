f9b99c550843627d861f30dbca68407cdbf664f5
MaGuowei <imaguowei@gmail.com>
2014-02-20 17:35:52 +0800

    update

--- sae/get_image.py
+++ sae/get_image.py
@@ -4,19 +4,22 @@
 [doc](http://sae.sina.com.cn/doc/python/storage.html)
 """
 
+from urllib import urlopen
 import tornado.wsgi
 import tornado.web
 import sae
 from sae.storage import Bucket
+from bing.image import BingImage
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         bucket = Bucket('image')
-        from urllib import urlopen
-        re = urlopen('http://s.cn.bing.net/az/hprichbg/rb/TianyuHan_ZH-CN8095317153_1366x768.jpg')
-        bucket.put_object('a.jpg', re)
-        self.write(bucket.generate_url('t.py'))
+        bing = BingImage(0, 1)
+        re = urlopen(bing.get_image())
+        image_name = str(bing.get_date()) + '.jpg'
+        bucket.put_object(image_name, re)
+        self.write(bucket.generate_url(image_name))
 
 
 app = tornado.wsgi.WSGIApplication([
