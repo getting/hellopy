bd1f08e77d85de879c37a60d35db093fc7dc038f
MaGuowei <imaguowei@gmail.com>
2014-03-30 21:20:51 +0800

    zhihu up

--- zhihu/zhihu.py
+++ zhihu/zhihu.py
@@ -3,16 +3,13 @@ from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 
 
+def get_html(url):
+    response = urlopen(url)
+    return response.read().decode()
+
+
 class Zhihu():
     url = 'http://www.zhihu.com/'
-
-    def __init__(self, username='', password=''):
-        self.username = username
-        self.password = password
-
-    def get_index_page(self):
-        response = urlopen(self.url)
-        return response.read().decode()
 
     def search(self, q):
         url = self.url + 'search?'
@@ -20,17 +17,34 @@ class Zhihu():
             'q': q,
             'type': 'question',
         }
-        response = urlopen(url + urlencode(query))
-        result = BeautifulSoup(response.read())
+
+        html = get_html(url + urlencode(query))
+        result = BeautifulSoup(html)
         questions = result.find_all('a', class_='question_link')
         for q in questions:
             print(q.em.next_sibling.string, q['href'])
 
 
-class Follower():
-    pass
+class User():
+    def __init__(self, username='', url=''):
+        self.username = username
+        self.url = url
+
+    def get_answers(self):
+        html = get_html(self.url + '/answers')
+        return html
+
+    def get_asks(self):
+        html = get_html(self.url + '/asks')
+        soup = BeautifulSoup(html)
+        asks = soup.find_all(class_='zm-profile-section-item zg-clear')
+        return asks
 
 
 if __name__ == '__main__':
-    zhihu = Zhihu()
-    print(zhihu.search('百度'))
+    # zhihu = Zhihu()
+    # print(zhihu.search('百度'))
+    user = User(url='http://www.zhihu.com/people/maguowei')
+    print(user.get_asks())
+
+
